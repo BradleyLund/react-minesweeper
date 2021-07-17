baa17bd7d0e2256aca1a01ff69b71fcaafb00e87
Bradley Lund <bradjlund@gmail.com>
2021-07-17 10:03:40 +0200

    Testing commiting from a different laptop

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -6,6 +6,7 @@ class Board extends React.Component {
     // we do a map within a map here, with each row of the matrix being its own array which creates
     // the game squares
     // We want to pass it the handleClick function
+    // testing to see if I can git push
 
     const gameSquares = this.props.gameMatrix.map((row, rowIndex) => (
       <div className="board-row">
