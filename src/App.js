822e35a96b5db2f7801abd4dc9aa0f5cceab331f
Bradley Lund <bradjlund@gmail.com>
2021-07-25 19:55:23 +0200

    add bombCount and clock and restart button styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import './App.css';
+import "./App.css";
 import React from "react";
-import Game from './Components/Game';
+import Game from "./Components/Game";
 
 class App extends React.Component {
-
   render() {
     return (
-      <div>
+      <div id="everything">
         <h2>Welcome to Minesweeper</h2>
         <Game />
       </div>
-
     );
   }
-
 }
 
 export default App;
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -86,6 +86,7 @@ class Game extends React.Component {
       elapsedTime: null,
       lost: null,
       won: null,
+      bombCount: 10,
     };
 
     this.handleClick = this.handleClick.bind(this);
@@ -123,9 +124,11 @@ class Game extends React.Component {
       // add the class of the flag
       square.classList.remove("hidden");
       square.classList.add("flag");
+      this.setState({ bombCount: this.state.bombCount - 1 });
     } else if (squareClassArray.indexOf("flag") !== -1) {
       square.classList.remove("flag");
       square.classList.add("hidden");
+      this.setState({ bombCount: this.state.bombCount + 1 });
     }
 
     // check if none of the squares have the hidden class then they have won
@@ -160,70 +163,78 @@ class Game extends React.Component {
     }
     // handle if it is a bomb
     let clickedSquare = document.getElementById(event.target.id);
-    if (event.target.textContent === "💣") {
-      // alert("You hit a bomb");
-      clickedSquare.classList.add("losing-bomb");
 
-      // loop through all of the squares and take away the hidden class
-      for (let i = 0; i < this.state.gameMatrix.length; i++) {
-        for (let j = 0; j < this.state.gameMatrix[i].length; j++) {
-          let square = document.getElementById(`${i}${j}`);
-          square.classList.remove("hidden");
+    // check if it has already been flagged so that it should not explode
+    if (!clickedSquare.classList.contains("flag")) {
+      if (event.target.textContent === "💣") {
+        // alert("You hit a bomb");
+        clickedSquare.classList.add("losing-bomb");
+
+        // loop through all of the squares and take away the hidden class
+        for (let i = 0; i < this.state.gameMatrix.length; i++) {
+          for (let j = 0; j < this.state.gameMatrix[i].length; j++) {
+            let square = document.getElementById(`${i}${j}`);
+            square.classList.remove("hidden");
+          }
         }
-      }
 
-      this.setState({ lost: true });
-      // pop up with restart as an option and then it just reloads the page?
-    } else if (event.target.textContent === "") {
-      // recursively open up all the tiles connected to this one that are also zero
-      // and open up all the tiles surrounding it
-      let gameMatrix = this.state.gameMatrix;
-      let alreadyChecked = [];
-      // create a function to change the class of those around it
-      function removeAround(i, j) {
-        // let i = locationId.split("")[0];
-        // let j = locationId.split("")[1];
-        console.log(gameMatrix, i, j, gameMatrix[i][j]);
-        console.log("what");
-        for (let k = -1; k < 2; k++) {
-          // console.log("is", k, gameMatrix[i + k], i + k);
-          if (gameMatrix[i + k] !== undefined) {
-            // console.log("hello");
-            for (let l = -1; l < 2; l++) {
-              if (gameMatrix[i + k][j + l] !== undefined) {
-                // remove the hidden class for the square with this id
-                let index1 = String(i + k);
-                let index2 = String(j + l);
+        this.setState({ lost: true });
+        // pop up with restart as an option and then it just reloads the page?
+      } else if (event.target.textContent === "") {
+        // recursively open up all the tiles connected to this one that are also zero
+        // and open up all the tiles surrounding it
+        let gameMatrix = this.state.gameMatrix;
+        let alreadyChecked = [];
+        // create a function to change the class of those around it
+        function removeAround(i, j) {
+          // let i = locationId.split("")[0];
+          // let j = locationId.split("")[1];
+          console.log(gameMatrix, i, j, gameMatrix[i][j]);
+          console.log("what");
+          for (let k = -1; k < 2; k++) {
+            // console.log("is", k, gameMatrix[i + k], i + k);
+            if (gameMatrix[i + k] !== undefined) {
+              // console.log("hello");
+              for (let l = -1; l < 2; l++) {
+                if (gameMatrix[i + k][j + l] !== undefined) {
+                  // remove the hidden class for the square with this id
+                  let index1 = String(i + k);
+                  let index2 = String(j + l);
 
-                let id = index1 + index2;
+                  let id = index1 + index2;
 
-                let square = document.getElementById(id);
+                  let square = document.getElementById(id);
 
-                square.classList.remove("hidden");
-                console.log(index1, index2, id, square);
+                  square.classList.remove("hidden");
+                  console.log(index1, index2, id, square);
 
-                //
-                if (
-                  gameMatrix[i + k][j + l] === 0 &&
-                  alreadyChecked.indexOf(id) === -1
-                ) {
-                  // submit it to the removeAround Function
-                  alreadyChecked.push(id);
-                  removeAround(i + k, j + l);
-                  // add this id to an array of already checked and if already checked,
-                  // don't run removearound for that square
+                  //
+                  if (
+                    gameMatrix[i + k][j + l] === 0 &&
+                    alreadyChecked.indexOf(id) === -1
+                  ) {
+                    // submit it to the removeAround Function
+                    alreadyChecked.push(id);
+                    removeAround(i + k, j + l);
+                    // add this id to an array of already checked and if already checked,
+                    // don't run removearound for that square
+                  }
                 }
               }
             }
           }
         }
-      }
 
-      console.log(event.target.id[0], event.target.id[1]);
-      // then while removing the classes if textContent of that id is nothing
-      // submit the id to the function
-      removeAround(parseInt(event.target.id[0]), parseInt(event.target.id[1]));
+        console.log(event.target.id[0], event.target.id[1]);
+        // then while removing the classes if textContent of that id is nothing
+        // submit the id to the function
+        removeAround(
+          parseInt(event.target.id[0]),
+          parseInt(event.target.id[1])
+        );
+      }
     }
+
     // testing to see if this works for changing of the class
 
     clickedSquare.classList.remove("hidden");
@@ -253,9 +264,12 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <div>{this.state.elapsedTime}</div>
-          <div>{this.state.lost ? "You lose!" : "Good luck!"}</div>
-          <button onClick={this.handleRestart}>Restart</button>
+          <div id="information">
+            <div>🕙 {this.state.elapsedTime}</div>
+            <button onClick={this.handleRestart}>Restart</button>
+            <div>💣{this.state.bombCount}</div>
+          </div>
+
           {/* pass down the state of the gameMatrix down to the board */}
           <Board
             gameMatrix={this.state.gameMatrix}
